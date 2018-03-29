98763a2642f427d51a5283d967d4502e04ef00cd
Riley O'Sheamail <osheari1@gmail.com>
2018-03-29 10:43:37 -0400

    Exercise 6

--- src/main/scala/3.scala
+++ src/main/scala/3.scala
@@ -132,13 +132,45 @@ object List { // `List` companion object. Contains functions for creating and wo
     case Cons(_,t) => Cons(h,t)
   }
 
+  //////////////////////////
+  // Exercise 6           //
+  // Keep all except last //
+  //////////////////////////
+  /*
+   Note that we're copying the entire list up until the last element.
+   Besides being inefficient, the natural recursive
+   solution will use a stack frame for each element of the list, which can lead to stack overflows for
+   large lists (can you see why?). With lists, it's common to use a temporary, mutable buffer internal to the
+   function (with lazy lists or streams, which we discuss in chapter 5, we don't normally do this). So long as the
+   buffer is allocated internal to the function, the mutation is not observable and RT is preserved.
+
+   Another common convention is to accumulate the output list in reverse order, then reverse it at the end, which
+   doesn't require even local mutation. We'll write a reverse function later in this chapter.
+   */
+  // def init[A](l: List[A]): List[A] = l match {
+  //   case Nil ⇒ sys.error("init on empty list")
+  //   case Cons(_, Nil) ⇒ l
+  //   case Cons(_, t) ⇒ init(t)
+  // }
   def init[A](l: List[A]): List[A] = l match {
     case Nil ⇒ sys.error("init on empty list")
-    case Cons(_, Nil) ⇒ l
-    case Cons(_, t) ⇒ init(t)
+    case Cons(_, Nil) ⇒ Nil
+    case Cons(h, t) ⇒ Cons(h, init(t))
+  }
+  def init2[A](l: List[A]): List[A] = {
+    import collection.mutable.ListBuffer
+    val buf = new ListBuffer[A]
+    @annotation.tailrec
+    def go(cur: List[A]): List[A] = cur match {
+      case Nil ⇒ sys.error("init of an empty list")
+      case Cons(_, Nil) ⇒ List(buf.toList: _*)  // Variadic function
+      case Cons(h, t) ⇒ buf += h; go (t)
+    }
+    go(l)
   }
 }
 
 val l = List(1, 2, 3, 4)
-List.init(l)
+List.init2(l)
+
 List.setHead(List(1, 2, 3, 4, 5), 2)
