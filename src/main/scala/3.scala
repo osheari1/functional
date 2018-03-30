d02137927a5369310d2d56669f82f210a1cb3772
Riley O'Sheamail <osheari1@gmail.com>
2018-03-29 20:29:33 -0400

    Exercise 13

--- src/main/scala/3.scala
+++ src/main/scala/3.scala
@@ -267,11 +267,42 @@ object List { // `List` companion object. Contains functions for creating and wo
   def reverse[A](l: List[A]): List[A] =
     foldLeft(l, List[A]())((acc, h) ⇒ Cons(h, acc))
 
+  //////////////////////////////////////////
+  // Exercise 13                          //
+  // Write foldLeft in terms of foldRight //
+  // Visa-versa                           //
+  //////////////////////////////////////////
+  /*
+   The implementation of `foldRight` in terms of `reverse` and `foldLeft` is a common trick for avoiding stack overflows
+   when implementing a strict `foldRight` function as we've done in this chapter. (We'll revisit this in a later chapter,
+   when we discuss laziness).
+
+   The other implementations build up a chain of functions which, when called, results in the operations being performed
+   with the correct associativity. We are calling `foldRight` with the `B` type being instantiated to `B => B`, then
+   calling the built up function with the `z` argument. Try expanding the definitions by substituting equals for equals
+   using a simple example, like `foldLeft(List(1,2,3), 0)(_ + _)` if this isn't clear. Note these implementations are
+   more of theoretical interest - they aren't stack-safe and won't work for large lists.
+   */
+  def foldRightViaFoldLeft[A, B](l: List[A], z: B)(f: (A, B) ⇒ B): B =
+    foldLeft(reverse(l), z)((b, a) ⇒ f(a, b))
+
+  def foldRightViaFoldLeft2[A, B](l: List[A], z: B)(f: (A, B) => B): B =
+    foldLeft(l, (b: B) ⇒ b)((g, a) ⇒ b ⇒ g(f(a, b)))(z)
+
+  def foldLeftViaFoldRight[A, B](l: List[A], z: B)(f: (B, A) ⇒ B): B =
+    foldRight(l, (b: B) ⇒ b)((a, g) ⇒ b ⇒ g(f(b, a)))(z)
+
+
+
+
+
 }
 
 val l = List( 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
 val l = List(1.0, 2.0, 3.0, 4.0, 5.0)
 val l = List(1, 2, 3, 4)
+List.foldLeftViaFoldRight(l, 0)(_+_)
+
 List.reverse(l)
 
 List.length(l)
