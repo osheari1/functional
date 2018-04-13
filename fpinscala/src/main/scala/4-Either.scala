c895367ebf5594429fe41b1d0413c487aeeeece6
Riley O'Sheamail <rileyoshea@fico.com>
2018-04-13 13:21:35 -0400

    Finish error handling

--- fpinscala/src/main/scala/4-Either.scala
+++ fpinscala/src/main/scala/4-Either.scala
@@ -63,9 +63,49 @@ object Either {
       case Nil ⇒ Right(Nil)
       case h :: t ⇒ (f(h) map2 (traverse(t)(f)))(_ :: _)
     }
-
+  def sequence[E, A](l: List[A]): Either[E, List[A]]=
+    traverse(l)(x ⇒ x)
 }
 
+
+case class Person(name: Name, age: Age)
+sealed class Name(val value: String)
+sealed class Age(val value: Int)
+
+def mkName(name: String): Either[String, Name] =
+  if (name == "" || name == null) Left("Name is empty.")
+  else Right(new Name(name))
+
+def mkAge(age: Int): Either[String, Age] =
+  if (age < 0) Left("Age out of range")
+  else Right(new Age(age))
+
+def mkPerson(name: String, age: Int): Either[String, Person] =
+  mkName(name).map2(mkAge(age))(Person(_, _))
+
+
+/*
+ There are a number of variations on `Option` and `Either`.
+ If we want to accumulate multiple errors, a simple
+ approach is a new data type that lets us keep a list of
+ errors in the data constructor that represents failures:
+
+ trait Partial[+A,+B]
+ case class Errors[+A](get: Seq[A]) extends Partial[A,Nothing]
+ case class Success[+B](get: B) extends Partial[Nothing,B]
+
+ There is a type very similar to this called `Validation` in the
+ Scalaz library. You can implement `map`, `map2`,
+ `sequence`, and so on for this type in such a way that errors are
+ accumulated when possible (`flatMap` is unable to
+ accumulate errors--can you see why?). This idea can even be generalized
+ further--we don't need to accumulate failing
+ values into a list; we can accumulate values using any user-supplied binary function.
+
+ It's also possible to use `Either[List[E],_]` directly to accumulate errors,
+ using different implementations of
+ helper functions like `map2` and `sequence`.
+ */
 
 // val l = IndexedSeq(1.0, 2.0, 3.0, 4.0, 5.0)
 // val l = IndexedSeq()
