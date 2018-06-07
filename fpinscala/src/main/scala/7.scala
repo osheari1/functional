83b6039e18e0a9385be67f64933af61e48341c88
Riley O'Shea <RileyOShea@corp.fairisaac.com>
2018-06-07 13:43:44 -0700

    Chapter 7 update

--- fpinscala/src/main/scala/7.scala
+++ fpinscala/src/main/scala/7.scala
@@ -123,10 +123,53 @@ object Par {
   }
 
 /*  Exercise 6*/
+  def parMap[A, B](l: List[A])(f: A => B): Par[List[B]] =
+    (es: ExecutorService) => {
+      val l2:List[B] = l.foldRight(List[B]())((x, xs) => f(x) :: xs)
+      UnitFuture(l2)
+    }
+
+  def sequence[A](as: List[Par[A]]): Par[List[A]] =
+    map(sequenceBalanced(as.toIndexedSeq))(_.toList)
+
+  def parMap2[A, B](l: List[A])(f: A => B): Par[List[B]] = {
+    val fbs: List[Par[B]] = l.map(asyncF(f))
+    sequence(fbs)
+  }
+
+// Exercise 7
+//    val l2: Par[List[A]] = l.foldRight(unit(List[A]()))((x, xs) => map2(x, unit(()))((x2, _) => ))
+  def sequence2[A](l: List[Par[A]]): Par[List[A]] =
+    l.foldRight[Par[List[A]]](unit(List()))((h, t) => map2(h, t)(_ :: _))
+
+  // This implementation forks the recursive step off to a new logical thread,
+  // making it effectively tail-recursive. However, we are constructing
+  // a right-nested parallel program, and we can get better performance by
+  // dividing the list in half, and running both halves in parallel.
+  // See `sequenceBalanced` below.
+  def sequenceRight[A](as: List[Par[A]]): Par[List[A]] =
+    as match {
+      case Nil => unit(Nil)
+      case h :: t => map2(h, fork(sequenceRight(t)))(_ :: _)
+    }
+
+  // We define `sequenceBalanced` using `IndexedSeq`, which provides an
+  // efficient function for splitting the sequence in half.
+  def sequenceBalanced[A](as: IndexedSeq[Par[A]]): Par[IndexedSeq[A]] = fork {
+    if (as.isEmpty) unit(Vector())
+    else if (as.length == 1) map(as.head)(a => Vector(a))
+    else {
+      val (l, r) = as.splitAt(as.length/2)
+      map2(sequenceBalanced(l), sequenceBalanced(r))(_ ++ _)
+    }
 
 
 
   }
+
+
+
+
 
 
 
